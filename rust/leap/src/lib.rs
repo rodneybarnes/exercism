c75661b0850012c5f6a64999f9e144f757cb3f38
Rodney Barnes <rodney@barnes.codes>
2022-01-29 14:57:21 -0500

    Add most starred solution

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,4 +1,9 @@
 pub fn is_leap_year(year: u64) -> bool {
+    most_starred_solution(year)
+}
+
+/// This was the solution I submitted
+fn my_solution(year: u64) -> bool {
     if year % 4 == 0 {
         if year % 100 == 0 {
             if year % 400 == 0 {
@@ -10,5 +15,15 @@ pub fn is_leap_year(year: u64) -> bool {
         return true
     } else {
         return false
+    }
+}
+
+/// fuuuck this is great
+fn most_starred_solution(year: u64) -> bool {
+    match (year % 4, year % 100, year % 400) {
+        (0, 0, 0) => true,
+        (0, 0, _) => false,
+        (0, _, _) => true,
+        (_, _, _) => false,
     }
 }
