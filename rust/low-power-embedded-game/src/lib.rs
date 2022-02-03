745c7f88e0cb93379de05a3f128433390d6e8424
Rodney Barnes <rodney@barnes.codes>
2022-02-03 15:02:22 -0500

    Improve implementation after seeing community solutions

--- rust/low-power-embedded-game/src/lib.rs
+++ rust/low-power-embedded-game/src/lib.rs
@@ -1,21 +1,14 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
-    let result = dividend / divisor;
-    let remainder = dividend % divisor;
-    (result, remainder)
+    (dividend / divisor, dividend % divisor)
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    iter.enumerate().filter(|i| i.0 % 2 == 0).map(|i| i.1)
+    iter.step_by(2)
 }
 
 pub struct Position(pub i16, pub i16);
 impl Position {
-    /// x1 - x0 + y1 - y0
     pub fn manhattan(&self) -> i16 {
-        (&self.0 - 0).abs() + (&self.1 - 0).abs()
+        &self.0.abs() + &self.1.abs()
     }
 }
