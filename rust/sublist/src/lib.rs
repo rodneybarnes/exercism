36cbc94417fedec7b802e5b6a44908ad8b41a7a5
Rodney Barnes <rbarnes@weeverapps.com>
2022-06-24 16:14:42 -0400

    Shamelessly steal most starred solution 😼

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -8,7 +8,19 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: Eq + Debug>(first_list: &[T], second_list: &[T]) -> Comparison {
+pub fn sublist<T: Eq>(a: &[T], b: &[T]) -> Comparison {
+    use Comparison::*;
+    match (a.len(), b.len()) {
+        (0, 0) => Equal,
+        (0, _) => Sublist,
+        (_, 0) => Superlist,
+        (m, n) if m > n => if a.windows(n).any(|v| v == b) {Superlist} else {Unequal},
+        (m, n) if m < n => if b.windows(m).any(|v| v == a) {Sublist} else {Unequal},
+        (_, _) => if a == b {Equal} else {Unequal},
+    }
+}
+
+pub fn my_sublist<T: Eq + Debug>(first_list: &[T], second_list: &[T]) -> Comparison {
     // if the first and second lists are the exact same, then return Comparison::Equal
     if first_list == second_list {
         return Comparison::Equal;
