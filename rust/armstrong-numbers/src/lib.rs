8730164fd4abe2acaca18037be12699044433b17
Rodney Barnes <rbarnes@weeverapps.com>
2022-08-05 14:32:08 -0400

    Add nice solution

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -5,3 +5,13 @@ pub fn is_armstrong_number(num: u32) -> bool {
         .fold(0, |acc, x| acc + x.to_digit(10).unwrap().pow(num.to_string().len().try_into().unwrap()));
     sum == num
 }
+
+pub fn one_of_the_most_starred_solutions(num: u32) -> bool {
+    let num_str = num.to_string();
+    let num_len = num_str.len() as u32;
+    let sum = num_str.chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .map(|d| d.pow(num_len))
+        .sum();
+    num == sum
+}
